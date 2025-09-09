840ea591301df1926833af6a8e6e63a7bbf1bdbd
Thundergoose <kaizenza@gmail.com>
2025-09-09 15:14:10 +1000

    Add org-code

--- components/login.tsx
+++ components/login.tsx
@@ -4,7 +4,10 @@ import { LoginLink } from "@kinde-oss/kinde-auth-nextjs/components";
 
 export function LoginButton() {
   return (
-    <LoginLink className="w-full py-2 px-4 rounded-md text-sm font-medium border bg-gray-50 hover:bg-gray-100 transition-colors">
+    <LoginLink
+      orgCode={process.env.NEXT_PUBLIC_KINDE_ORG_CODE}
+      className="w-full py-2 px-4 rounded-md text-sm font-medium border bg-gray-50 hover:bg-gray-100 transition-colors"
+    >
       Login
     </LoginLink>
   );
--- components/signup.tsx
+++ components/signup.tsx
@@ -4,7 +4,10 @@ import { RegisterLink } from "@kinde-oss/kinde-auth-nextjs/components";
 
 export function SignUpButton() {
   return (
-    <RegisterLink className="w-full py-2 px-4 rounded-md text-sm font-medium border bg-gray-50 hover:bg-gray-100 transition-colors">
+    <RegisterLink
+      orgCode={process.env.NEXT_PUBLIC_KINDE_ORG_CODE}
+      className="w-full py-2 px-4 rounded-md text-sm font-medium border bg-gray-50 hover:bg-gray-100 transition-colors"
+    >
       Sign Up
     </RegisterLink>
   );
